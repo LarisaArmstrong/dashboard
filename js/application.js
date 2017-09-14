213eb13db05270d0ef2ece82055883f718e415a8
LarisaArmstrong <larisaarmstrong@Larisas-MacBook-Pro.local>
2017-09-14 10:31:11 +0200

    add background image

--- js/application.js
+++ js/application.js
@@ -1,18 +1,18 @@
-var templates = [
-	{name: 'Dark', value: 'css/dark.css'},
-  {name: 'Light', value: 'css/light.css'}
-]
-
-var app = new Vue ({
-	el: '#app',
-	data: {
-		title: "Title",
-		templates: {
-			list: templates,
-			can_list: templates.length > 1
-		}
-	}
-})
+//var templates = [
+//	{name: 'Dark', value: 'css/dark.css'},
+//  {name: 'Light', value: 'css/light.css'}
+//]
+//
+//var app = new Vue ({
+//	el: '#app',
+//	data: {
+//		title: "Title",
+//		templates: {
+//			list: templates,
+//			can_list: templates.length > 1
+//		}
+//	}
+//})
 
 
 
