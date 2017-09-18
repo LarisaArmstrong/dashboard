7a732c16717dbc7459d531d7a5d283686158f60f
LarisaArmstrong <larisaarmstrong@Larisas-MacBook-Pro.local>
2017-09-18 12:24:13 +0200

    added base and css files
    imported and linked to stylesheet correct way

--- js/application.js
+++ js/application.js
@@ -11,30 +11,7 @@ $(document).ready(function() {
     
 });    
     
-    
 
-
-
-
-
-
-
-
-//var templates = [
-//	{name: 'Dark', value: 'css/dark.css'},
-//  {name: 'Light', value: 'css/light.css'}
-//]
-//
-//var app = new Vue ({
-//	el: '#app',
-//	data: {
-//		title: "Title",
-//		templates: {
-//			list: templates,
-//			can_list: templates.length > 1
-//		}
-//	}
-//})
 
 
 
