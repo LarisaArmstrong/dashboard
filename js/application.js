fe833e5fe7c175442fa277ee5b0e6340d16d60d0
LarisaArmstrong <larisaarmstrong@Larisas-MacBook-Pro.local>
2017-09-19 11:27:59 +0200

    added variables in scss
    light and dark themes working but not reacting yet

--- js/application.js
+++ js/application.js
@@ -13,6 +13,48 @@ $(document).ready(function() {
 
 
 
+// login fake database
+
+var users = [
+    { id: 1, name: 'john', value: '', logo: ''},]
+
+
+var templates = [
+  { id: 1, name: 'Dark', value: 'css/dark.css', logo: 'css/dark.css'},
+  { id: 2, name: 'Light', value: 'css/light.css', logo: 'css/light.css'}
+];
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: "Hello, VueJS",
+    users: users,
+    templates: templates,
+    default_template: 2,
+    current_template: templates[0]
+  },
+  methods: {
+    setTemplate: function(id){
+      var selected_template = this.templates.filter(function(ele){
+        return (ele.id == id)
+      })[0];
+
+      this.current_template = selected_template;
+    },
+    tableHeader: function(tableData) {
+      var keys = [];
+      for(key in tableData[0]){
+        keys.push(key);
+      }
+      return keys;
+    }
+  }
+});
+
+
+
+
 
 
 
